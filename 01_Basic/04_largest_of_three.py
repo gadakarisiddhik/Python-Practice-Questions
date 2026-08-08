12462c058050e307ff913b6d1d0351d04b0ab4c6
Siddik Imran Gadkari <gadakarisiddhik@gmail.com>
2026-08-08 17:04:11 +0530

    Added Find Largest of Three Qus

---
+++ 01_Basic/04_largest_of_three.py
@@ -0,0 +1,10 @@
+n = int(input("Enter a Frist no: "))
+m = int(input("Enter a Frist no: "))
+o = int(input("Enter a Frist no: "))
+
+if n > m and n> o :
+    print(n,"is Largest")
+elif m > n and m > o:
+    print(m,"is Largest")
+else:
+    print(o,"is Largest")
